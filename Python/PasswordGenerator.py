f94527906b4da1810d41fcf6df6855dc3da6c531
Admin <progoodbro@gmail.com>
2021-12-28 13:19:27 +0300

    Почти конечная версия

--- Python/PasswordGenerator.py
+++ Python/PasswordGenerator.py
@@ -1,80 +1,48 @@
 # Написать програмку генерации паролей для пользователя.
 # Пользователь может выбрать какого типа символы нужны для пароля: цифры, буквы прописные/строчные, спецсимволы (можно выбрать какие).
 
+'D:\\Users\\Admin\Desktop\\config.txt'
 import random
 import string
+import sys
+import os.path
 
-def Pass(conditions, removeElement, lengthPassword) : 
-   joinConditions = ''.join(conditions)
-   elements = list(joinConditions)
-   for j in list(removeElement) :
-      if elements.count(j) > 0 :
-         elements.remove(j)
-      else :
-         continue
-   password = ''.join([random.choice(elements) for i in range(lengthPassword)])
+defaultDict = {
+   'length' : 10,
+   'symbols' : 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+}
+
+def Passwd(symbols,lengthPassword) : 
+   print('symbols =',symbols)
+   print('lengthPassword =',lengthPassword)
+   elements = list(symbols)
+   password = ''.join([random.choice(elements) for i in range(0, int(lengthPassword))])
    return password
 
-A = string.ascii_lowercase + 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-U = string.ascii_uppercase + 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
-D = string.digits
-P = string.punctuation
-lengthPassword = input('Количество символов: ')
-print('A - Строчные буквы (', A ,')' )
-print('U - Прописные буквы (', U ,')' )
-print('D - Цифры (' , D , ')' )
-print('P - Символы пунктуации ( !"#$%&''()*+,-./:;<=>?@[\]^_{|}~ )')
-conditions = input('Введите буквы, которые соответствуют типу символов и будут содержаться в пароле :')
-removeElement = input('Введите символ которого не будет в пароле :')
-conditions = conditions.upper()
-if conditions.count('A')!=0 and conditions.count('U')!=0 and conditions.count('D')!=0 and conditions.count('P')!=0 :
-   conditions = [A, U, D, P]
-   print(Pass(conditions, removeElement,lengthPassword))
-elif conditions.count('A')!=0 and conditions.count('U')!=0 and conditions.count('D')!=0 and conditions.count('P')<=0 :
-   conditions = [A, U, D]
-   print(Pass(conditions, removeElement,lengthPassword))
-elif conditions.count('A')!=0 and conditions.count('U')!=0 and conditions.count('D')<=0 and conditions.count('P')<=0 :
-   conditions = [A, U]
-   print(Pass(conditions, removeElement,lengthPassword))
-elif conditions.count('A')!=0 and conditions.count('U')<=0 and conditions.count('D')<=0 and conditions.count('P')<=0 :
-   conditions = [A]
-   print(Pass(conditions, removeElement,lengthPassword))
-elif conditions.count('A')!=0 and conditions.count('U')<=0 and conditions.count('D')!=0 and conditions.count('P')!=0 :
-   conditions = [A, D, P]
-   print(Pass(conditions, removeElement,lengthPassword))
-elif conditions.count('A')!=0 and conditions.count('U')<=0 and conditions.count('D')<=0 and conditions.count('P')!=0 :
-   conditions = [A, P]
-   print(Pass(conditions, removeElement,lengthPassword))
-elif conditions.count('A')!=0 and conditions.count('U')<=0 and conditions.count('D')!=0 and conditions.count('P')<=0 :
-   conditions = [A, D]
-   print(Pass(conditions, removeElement,lengthPassword))
-elif conditions.count('A')!=0 and conditions.count('U')!=0 and conditions.count('D')<=0 and conditions.count('P')!=0 :
-   conditions = [A, U, P]
-   print(Pass(conditions, removeElement,lengthPassword))
-elif conditions == 'PU' or conditions == 'UP' :
-   conditions = [U, P]
-   print(Pass(conditions, removeElement,lengthPassword))
-elif conditions == 'U' :
-   conditions = [U]
-   print(Pass(conditions, removeElement,lengthPassword))
-elif conditions == 'P' :
-   conditions = [P]
-   print(Pass(conditions, removeElement,lengthPassword))
-elif conditions == 'D' :
-   conditions = [D]
-   print(Pass(conditions, removeElement,lengthPassword))
-elif conditions == 'PUD' or conditions == 'UPD' or conditions == 'DUP':
-   conditions = [P, U, D]
-   print(Pass(conditions, removeElement,lengthPassword))
-elif conditions == 'PD' or conditions == 'DP':
-   conditions = [P, D]
-   print(Pass(conditions, removeElement,lengthPassword))
-elif conditions == 'UD' or conditions == 'DU':
-   conditions = [U, D]
-   print(Pass(conditions, removeElement,lengthPassword))
-elif conditions == 'PUD' or conditions == 'UPD' or conditions == 'DUP':
-   conditions = [P, U, D]
-   print(Pass(conditions, removeElement,lengthPassword))
-else :
-   print('Ошибка! Введены неверные данные!')
+def ReadFile(failPath) : 
+   file = open(failPath, encoding='utf-8')
+   for i in range(0, 2) :
+      list1 = file.readline()
+      index = list1.index('=')
+      if index != 0 :
+         if list1[:index] in defaultDict :
+            defaultDict[list1[:index]] = list1[index+1:-1]
+   file.close()
+   print(Passwd(defaultDict['symbols'], defaultDict['length']))
+
+a = len(sys.argv)
+print(a)
+if a == 1 :
+   print(Passwd(defaultDict['symbols'],defaultDict['length']))
+elif a == 2 :
+   b = os.path.isfile(sys.argv[1])
+   if b :
+      failPath = sys.argv[1]
+      ReadFile(failPath)
+   else :
+      print('Параметров нет')
+      
+# failPath = r'D:\Users\Admin\Desktop\config.txt'
+# ReadFile(failPath)
+
 
